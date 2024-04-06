6d8b909dbf4651f5ba3dff23e36361a5f16274e4
TaylorBellReal <112988602+TaylorBellReal@users.noreply.github.com>
2024-04-05 19:39:24 -0500

    Added-better-walls

--- ball.gd
+++ ball.gd
@@ -1,10 +1,4 @@
 extends RigidBody2D
 
 func _physics_process(delta):
-	var collisions = get_overlapping_bodies()
-	for collision in collisions:
-		if collision is Label:
-			# Get the normal vector of the collision
-			var normal = get_slide_collision_normal(collision)
-			# Reflect the linear velocity based on the normal
-			bounce(reflect(linear_velocity, normal))
+	pass
