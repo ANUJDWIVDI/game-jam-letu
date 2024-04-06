b7d285d8c4e4973a4b2dce6f80b222b797351961
TaylorBellReal <112988602+TaylorBellReal@users.noreply.github.com>
2024-04-06 01:06:29 -0500

    Menu-minor-ball-changes

--- Panel.gd
+++ Panel.gd
@@ -1,62 +1,28 @@
 extends Panel
 
-# List of menu options
-var menuOptions = ["New Game", "Load Game", "Options", "Quit", "Rules", "Volume Toggle"]
+var packed_scene = "res://Pinball.tscn"
 
-func _ready():
-	# Calculate vertical space between buttons
-	var button_height = rect_size.y / menuOptions.size()
 
-	# Iterate through menu options and create buttons
-	for i in range(menuOptions.size()):
-		var button = Button.new()
-		button.text = menuOptions[i]
-		button.rect_min_size.y = button_height
-		button.rect_anchor_vertical = 0.5
-		button.rect_position.y = i * button_height
-		button.connect("pressed", self, "_on_button_pressed", [i])
-		add_child(button)
-
-# Called when a menu button is pressed
-func _on_button_pressed(index):
-	match index:
-		0: # New Game
-			_on_new_game_pressed()
-		1: # Load Game
-			_on_load_game_pressed()
-		2: # Options
-			_on_options_pressed()
-		3: # Quit
-			_on_quit_pressed()
-		4: # Rules
-			_on_rules_pressed()
-		5: # Volume Toggle
-			_on_volume_toggle_pressed()
-
-# Start a new game
-
-	
-# Start a new game
 func _on_new_game_pressed():
-	get_tree().change_scene("res://Pinball.tscn")
+	get_tree().change_scene_to_file(packed_scene)
+	pass # Replace with function body.
 
 
-# Load a game
 func _on_load_game_pressed():
-	print("Game loaded") # Replace with your own logic
+	pass # Replace with function body.
 
-# Show rules
+
 func _on_rules_pressed():
-	print("Rules shown") # Replace with your own logic
+	pass # Replace with function body.
 
-# Show options
+
 func _on_options_pressed():
-	print("Options shown") # Replace with your own logic
+	pass # Replace with function body.
 
-# Quit the game
+
 func _on_quit_pressed():
-	get_tree().quit()
+	pass # Replace with function body.
 
-# Toggle volume
+
 func _on_volume_toggle_pressed():
-	print("Volume toggled") # Replace with your own logic
+	pass # Replace with function body.
