d7d696c38e159d45f6a21719fdfbd893e4f35fc4
ANUJDWIVD <anujd0009@gmail.com>
2024-04-05 21:40:37 -0500

    respawn-part-success

--- ball.gd
+++ ball.gd
@@ -2,15 +2,24 @@ extends RigidBody2D
 
 var velocity = Vector2.ZERO
 var physics_material = PhysicsMaterial.new()
-
+var initial_position = Vector2(1113, 258)
+var counter = 0
 
 func _ready():
 	physics_material.bounce = 2.0
 	self.physics_material_override = physics_material
 
 func _physics_process(delta):
-#	move_and_slide(velocity)
-	pass
+
+	if position.y > 600:
+		position = initial_position
+		velocity = Vector2.ZERO
+		counter += 1
+	else:
+		#move_and_slide(velocity)
+		pass
+
+
 
 #func apply_central_impulse(impulse):
 #	velocity += impulse
